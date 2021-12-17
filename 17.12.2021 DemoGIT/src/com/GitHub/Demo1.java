26e0515e4019ba1331f92801a31ccb9610666191
user <user@player.kmpmedia.net>
2021-12-17 16:16:23 +0530

    by kadam

--- 17.12.2021 DemoGIT/src/com/GitHub/Demo1.java
+++ 17.12.2021 DemoGIT/src/com/GitHub/Demo1.java
@@ -2,8 +2,16 @@ package com.GitHub;
 
 public class Demo1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("By....");
 		System.out.println("Hiii..");
+		
+		
+	}
+
+	public void m1()
+	{
+		System.out.println("Hii Akash");
 	}
 }
